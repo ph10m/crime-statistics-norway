0cccd7d3db2081c5c57d80c3976b58e078914eef
laaaders <larsmade@stud.ntnu.no>
2017-10-31 10:47:37 +0100

    Minor change

--- web/src/app/new-user/new-user.component.ts
+++ web/src/app/new-user/new-user.component.ts
@@ -17,34 +17,39 @@ export class NewUserComponent implements OnInit {
   }
 
 //Onclick from newUser, takes in arguments, should check them against the database.  
-  newUserClick(mail: string, pass1: string, pass2: string){
-    this.errorMessage = [];
-    this.error = false;
+  logInClick(mail: string, pass: string){
+    console.log(mail)
+    console.log(pass)
+  }
 
-    //Check email field. 
-    if(mail.length == 0){
-      this.error = true
-      this.errorMessage.push("Must have email");
-    }else{
-      //Check if email adress. 
-    if(this.validateEmail(mail) == false){
-      this.errorMessage.push("Not an email adress");
-      this.error = true
-    }
-    }
-    //Check password fields
-    if(pass1 != pass2){
-      this.errorMessage.push("Passwords do not match");
-      this.error = true
-    }else{
-      if(pass1.length <= 6){
-        this.errorMessage.push("Password is shorter than 7 charachters");
-        this.error = true
-      }
-  }
-  }
-  validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
+    
+  //   this.errorMessage = [];
+  //   this.error = false;
+
+  //   //Check email field. 
+  //   if(mail.length == 0){
+  //     this.error = true
+  //     this.errorMessage.push("Must have email");
+  //   }else{
+  //     //Check if email adress. 
+  //   if(this.validateEmail(mail) == false){
+  //     this.errorMessage.push("Not an email adress");
+  //     this.error = true
+  //   }
+  //   }
+  //   //Check password fields
+  //   if(pass1 != pass2){
+  //     this.errorMessage.push("Passwords do not match");
+  //     this.error = true
+  //   }else{
+  //     if(pass1.length <= 6){
+  //       this.errorMessage.push("Password is shorter than 7 charachters");
+  //       this.error = true
+  //     }
+  // }
+  // }
+  // validateEmail(email) {
+  //   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  //   return re.test(email);
+  // }
 }
