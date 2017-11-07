b8a8718c0ccaab5b997d4bd8bb5018892426a46c
Kristian Langvann <>
2017-11-07 13:54:10 +0100

    fixed merge conflicts ready to merge with master

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http'; //https://medium.com/codingthesmartway-com-blog/angular-4-3-httpclient-accessing-rest-web-services-with-angular-2305b8fd654b
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+//import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 //Main component and Routing. 
@@ -18,15 +17,11 @@ import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { NewUserComponent } from './new-user/new-user.component';
 import { MySiteComponent } from './my-site/my-site.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+
 
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MunicipalityDetailComponent } from './fetch-data/munic-detail.component';
-//import { SearchComponent } from './fetch-data/search.component';
-//import { GithubAuthInterceptor } from './fetch-data/githubauth.interceptor';
-
-//lars
-import { SearchResultComponent } from './search-result/search-result.component';
-
 
 
 @NgModule({
@@ -36,11 +31,10 @@ import { SearchResultComponent } from './search-result/search-result.component';
     LogInComponent,
     NewUserComponent,
     MySiteComponent,
-
+    SearchResultComponent,
+    
     FetchDataComponent,
     MunicipalityDetailComponent,
-    //SearchComponent
-    SearchResultComponent,
   ],
   imports: [
     BrowserModule,
--- web/src/app/fetch-data/munic-detail.component.ts
+++ web/src/app/fetch-data/munic-detail.component.ts
@@ -7,50 +7,53 @@ import { Municipality } from './municipality';
     <div *ngIf="municipality">
       <h2>{{municipality[1]}}!</h2>
       <div>
-        <label>id: </label>{{municipality[0]}}
+        <label>Id: </label>{{municipality[0]}}
       </div>
       <div>
-        <label>name: </label>{{municipality[1]}}
+        <label>Name: </label>{{municipality[1]}}
       </div>
       <div>
         <div> 
-        <label>Absolutte tall: </label>{{municipality[2]}}
+        <label>Total crimes: </label>{{municipality[2]}}
         </div> 
         <div>
-        <label>Property 1000: </label>{{municipality[3]}} 
+        <label>Total per 1000: </label>{{municipality[3]}} 
         </div>
         <div> 
-        <label>name: </label>{{municipality[4]}}
+        <label>Property total: </label>{{municipality[4]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[5]}}
+        <label>Property per 1000: </label>{{municipality[5]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[6]}}
+        <label>Violence total: </label>{{municipality[6]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[7]}}
+        <label>Violence per 1000: </label>{{municipality[7]}}
          </div>
         <div>
-        <label>name: </label>{{municipality[8]}}
+        <label>Drugs total: </label>{{municipality[8]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[9]}}
+        <label>Drugs per 1000: </label>{{municipality[9]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[10]}}
+        <label>Order total: </label>{{municipality[10]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[11]}} 
+        <label>Order per 1000: </label>{{municipality[11]}} 
         </div>
         <div>
-        <label>name: </label>{{municipality[12]}}
+        <label>Traffic total: </label>{{municipality[12]}}
         </div>
         <div>
-        <label>name: </label>{{municipality[13]}} 
+        <label>Traffic per 1000: </label>{{municipality[13]}} 
         </div>
         <div>
-        <label>name: </label>{{municipality[14]}}
+        <label>Other total: </label>{{municipality[14]}}
+        </div>
+        <div>
+        <label>Other per 1000: </label>{{municipality[15]}}
         </div>
       </div>
     </div>
