2b673ce74c5d81887ad25363f0c5a8c04a4988cf
Magnus Guttormsen <magnugut@stud.ntnu.no>
2017-11-13 11:22:09 +0100

    added limit 10

--- web/server/models/search.js
+++ web/server/models/search.js
@@ -1,7 +1,7 @@
 var db = require("../db.js").getConnection();
 
 exports.search = function(name, cb) {
-    db.all("SELECT * FROM Crimestat WHERE municipacility LIKE '%" + name + "%'", function(err, row) {
+    db.all("SELECT * FROM Crimestat WHERE municipacility LIKE '%" + name + "%' limit 10", function(err, row) {
         if (row == undefined) {
             cb(false);
         }
