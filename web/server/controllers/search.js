ceec576e6c74903adcced0cde508e0f7fe0e21b4
Magnus Guttormsen <magnugut@stud.ntnu.no>
2017-11-18 09:45:42 +0100

    started on search

--- web/server/controllers/search.js
+++ web/server/controllers/search.js
@@ -1,10 +1,10 @@
-var express = require('express')
-, router = express.Router()
-, path = require('path')
-, crimestat = require(path.normalize('../models/search'));
+var express = require('express'),
+    router = express.Router(),
+    path = require('path'),
+    crimestat = require(path.normalize('../models/search'));
 
 router.post('/search', function(req, res) {
-    crimestat.search(req.body.name, function(crimes) {
+    crimestat.search(req.body.name, req.body.sort, req.body.sortAscDesc, function(crimes) {
         res.json({
             crimes
         })
--- web/server/models/search.js
+++ web/server/models/search.js
@@ -1,13 +1,53 @@
 var db = require("../db.js").getConnection();
 
-exports.search = function(name, cb) {
-    db.all("SELECT * FROM Crimestat WHERE municipacility LIKE '%" + name + "%' limit 10", function(err, row) {
-        if (row == undefined) {
-            cb(false);
+exports.search = function(name, sort, sortAscDesc, cb) {
+
+    var sqlQuery = "SELECT * FROM Crimestat";
+
+    if (name != undefined) {
+        name = "%" + name + "%";
+        sqlQuery += " WHERE municipacility LIKE ?";
+    }
+
+    if (sort != undefined) {
+        if (sortAscDesc != undefined) {
+            sqlQuery += " ORDER BY ? ASC";
+        } else {
+            sqlQuery += " ORDER BY ? DESC";
         }
-        else {
+    }
+
+    console.log("sql query " + sqlQuery)
+
+    db.all(sqlQuery, name, sort, sortAscDesc, function(err, row) {
+        if (row == undefined) {
+            console.log("error " + err)
+            cb(false);
+        } else {
             cb(row);
         }
     });
 
+
+
+
+
+
+    /* if (sort != undefined) {
+         db.all("SELECT * FROM Crimestat WHERE municipacility LIKE '%" + name + "%' limit 10", function(err, row) {
+             if (row == undefined) {
+                 cb(false);
+             } else {
+                 cb(row);
+             }
+         });
+     } else {
+         db.all("SELECT * FROM Crimestat WHERE municipacility LIKE '%" + name + "%' limit 10", function(err, row) {
+             if (row == undefined) {
+                 cb(false);
+             } else {
+                 cb(row);
+             }
+         });
+     }*/
 }
