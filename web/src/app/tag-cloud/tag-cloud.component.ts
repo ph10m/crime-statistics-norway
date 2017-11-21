43e98265566fcdfc8c44f026aedd230549fd328f
Ola Richard Syversen <olasy95@gmail.com>
2017-11-21 12:01:56 +0100

    fixed fuckup

--- web/src/app/tag-cloud/tag-cloud.component.ts
+++ web/src/app/tag-cloud/tag-cloud.component.ts
@@ -3,7 +3,7 @@ import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-import * as $ from "jquery";
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-tag-cloud',
@@ -11,18 +11,17 @@ import * as $ from "jquery";
   styleUrls: ['./tag-cloud.component.css'],
 })
 
-// class that that is used to create the component used for the word clouds
-// the way we chose to implement it has 7 different cloud objects rendered, and only showing 1 at a time,
-// instead of having 1 cloud object that changes its data based on what is selected. 
-// this was done because with the amount of data used in the clouds the loading times were far too long (over a minute)
-// with only 1 component, and just about 10 seconds with 7 components.
+/* class that that is used to create the component used for the word clouds
+the way we chose to implement it has 7 different cloud objects rendered, and only showing 1 at a time,
+instead of having 1 cloud object that changes its data based on what is selected. 
+this was done because with the amount of data used in the clouds combined with the tag cloud library, 
+the loading times were far too long (over a minute) with only 1 component,
+and just about 10 seconds with 7 components. Which still is a lot, but it was the best possible working
+solution we found. */
 export class TagCloudComponent implements OnInit {
   private req: any;
-
   retrieved: any;
-
   type = 'all';
-
   url = 'http://www.google.com/search?q=';
 
   options: CloudOptions = {
@@ -32,6 +31,7 @@ export class TagCloudComponent implements OnInit {
     overflow: false,
   }
 
+  // initializes all the clouds
   data1: Array<CloudData> = [
     { text: 'Loading...', weight: 10 },
   ]
@@ -63,16 +63,17 @@ export class TagCloudComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
+  // retrieves the data from the DB and sends it to the changeData function
   ngOnInit() {
-
     this.req = this.http.get('http://localhost:8084/cloud/cloudData').subscribe(data => {
-      this.retrieved = data
+      this.retrieved = data;
       this.changeData(this.retrieved);
       this.req.unsubscribe();
     })
 
   }
 
+  // function used onclick to change between the various word clouds, as well as style the apropriate buttons
   choose(event, type) {
     this.type = type;
     $('.selected').removeClass('selected');
@@ -80,6 +81,7 @@ export class TagCloudComponent implements OnInit {
   }
 
 
+  // retrieves new data to use in the clouds, formats the data to CloudData objects and updates the clouds
   changeData(newData) {
     let parsedData1 = [];
     let parsedData2 = [];
@@ -90,32 +92,25 @@ export class TagCloudComponent implements OnInit {
     let parsedData7 = [];
 
     for (let i = 0; i < newData.length; i++) {
-      parsedData1.push({ 'text': newData[i].text, 'weight': newData[i].all,
-       'link': this.url + String(newData[i].text)});
+      parsedData1.push({ 'text': newData[i].text, 'weight': newData[i].all, 'link': this.url +  newData[i].text});
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData2.push({ 'text': newData[i].text, 'weight': newData[i].property,
-       'link': this.url + String(newData[i].text)});
+      parsedData2.push({ 'text': newData[i].text, 'weight': newData[i].property, 'link': this.url +  newData[i].text });
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData3.push({ 'text': newData[i].text, 'weight': newData[i].violence,
-       'link': this.url + String(newData[i].text)});
+      parsedData3.push({ 'text': newData[i].text, 'weight': newData[i].violence, 'link': this.url +  newData[i].text });
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData4.push({ 'text': newData[i].text, 'weight': newData[i].drugs,
-       'link': this.url + String(newData[i].text)});
+      parsedData4.push({ 'text': newData[i].text, 'weight': newData[i].drugs, 'link': this.url +  newData[i].text });
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData5.push({ 'text': newData[i].text, 'weight': newData[i].order,
-       'link': this.url + String(newData[i].text)});
+      parsedData5.push({ 'text': newData[i].text, 'weight': newData[i].order, 'link': this.url +  newData[i].text });
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData6.push({ 'text': newData[i].text, 'weight': newData[i].traffic,
-       'link': this.url + String(newData[i].text)});
+      parsedData6.push({ 'text': newData[i].text, 'weight': newData[i].traffic, 'link': this.url +  newData[i].text });
     }
     for (let i = 0; i < newData.length; i++) {
-      parsedData7.push({ 'text': newData[i].text, 'weight': newData[i].other,
-       'link': this.url + String(newData[i].text)});
+      parsedData7.push({ 'text': newData[i].text, 'weight': newData[i].other, 'link': this.url +  newData[i].text });
     }
 
     let changedData1$: Observable<Array<CloudData>> = Observable.of(parsedData1);
